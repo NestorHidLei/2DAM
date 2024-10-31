d61765b93ecdb75dd6134fcd2ed981dab51105ad
Nestor <nhidlei@gmail.com>
2024-10-31 17:05:00 +0100

    AADD terminado

--- Tarea 7. CRUD con JDBC/src/main/java/dao/CityDaoImpl.java
+++ Tarea 7. CRUD con JDBC/src/main/java/dao/CityDaoImpl.java
@@ -2,64 +2,118 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import conexion.Conexion;
 import model.City;
 
-public class CityDaoImpl implements CityDao{
+public class CityDaoImpl implements CityDao {
 
 	private static CityDaoImpl instance;
-	
+
 	static {
 		instance = new CityDaoImpl();
 	}
-	
-	private CityDaoImpl() {}
-	
-	public static CityDaoImpl getInstance(){
+
+	private CityDaoImpl() {
+	}
+
+	public static CityDaoImpl getInstance() {
 		return instance;
 	}
-	
-	
+
 	@Override
 	public int add(City city) throws SQLException {
 		String sql = "INSERT INTO City(name, countryCode, district, population) VALUES(?,?,?,?);";
-		
-		try(Connection conn = Conexion.conectar();
-			PreparedStatement pstm = conn.prepareStatement(sql)){
+
+		try (Connection conn = Conexion.conectar(); PreparedStatement pstm = conn.prepareStatement(sql)) {
 			pstm.setString(1, city.getName());
 			pstm.setString(2, city.getCountryCode());
 			pstm.setString(3, city.getDistrict());
 			pstm.setInt(4, city.getPopulation());
-			
+
 			return pstm.executeUpdate();
 		}
 	}
 
 	@Override
 	public City getById(int id) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM city where ID = ?;";
+		City result = null;
+
+		try (Connection conn = Conexion.conectar(); PreparedStatement pstm = conn.prepareStatement(sql);) {
+			pstm.setInt(1, id);
+
+			try (ResultSet rs = pstm.executeQuery()) {
+				while (rs.next()) {
+					result = new City();
+					result.setId(rs.getInt("ID"));
+					result.setName(rs.getString("Name"));
+					result.setCountryCode(rs.getString("CountryCode"));
+					result.setDistrict(rs.getString("District"));
+					result.setPopulation(rs.getInt("Population"));
+				}
+			}
+
+		}
+
+		return result;
 	}
 
 	@Override
 	public List<City> getAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM city";
+		List<City> result = new ArrayList<City>();
+
+		try (Connection conn = Conexion.conectar();
+				PreparedStatement pstm = conn.prepareStatement(sql);
+				ResultSet rs = pstm.executeQuery()) {
+			City city;
+			while (rs.next()) {
+				city = new City();
+				city.setId(rs.getInt("ID"));
+				city.setName(rs.getString("Name"));
+				city.setCountryCode(rs.getString("CountryCode"));
+				city.setDistrict(rs.getString("District"));
+				city.setPopulation(rs.getInt("Population"));
+				result.add(city);
+			}
+		}
+
+		return result;
 	}
 
 	@Override
 	public int update(City city) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "UPDATE city SET Name = ?, CountryCode = ?, District = ?, Population = ? WHERE ID = ?;";
+		
+		int result;
+		
+		try(Connection conn = Conexion.conectar();
+				PreparedStatement pstm = conn.prepareStatement(sql)){
+			pstm.setString(1, city.getName());
+			pstm.setString(2, city.getCountryCode());
+			pstm.setString(3, city.getDistrict());
+			pstm.setInt(4, city.getPopulation());
+			pstm.setInt(5, city.getId());
+			
+			result = pstm.executeUpdate();
+		}
+		
+		return result;
 	}
 
 	@Override
 	public void delete(int id) throws SQLException {
-		// TODO Auto-generated method stub
-		
+		String sql = "DELETE FROM city where ID = ?;";
+		try(Connection conn = Conexion.conectar();
+				PreparedStatement pstm = conn.prepareStatement(sql)){
+			pstm.setInt(1, id);
+			pstm.executeUpdate();
+		}
 	}
 
 }
--- Tarea 7. CRUD con JDBC/src/main/java/mainApp/MainApp.java
+++ Tarea 7. CRUD con JDBC/src/main/java/mainApp/MainApp.java
@@ -1,31 +1,100 @@
 package mainApp;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import dao.CityDao;
 import dao.CityDaoImpl;
 import model.City;
 
 public class MainApp {
-	
+
 	public static void main(String[] args) throws Exception {
-		testDao();
-		
+//		testDaoAñadir();
+//		testDaoListar();
+//		testDaoGetId();
+//		testUpdate();
+//		testBorrar();
 	}
-	
-	public static void testDao() {
+
+	public static void testDaoAñadir() {
 		CityDao dao = CityDaoImpl.getInstance();
-		
+
 		City city = new City("Ciudad Nueva 1", "ESP", "Andalusia", 1200);
-		
+
 		try {
 			int n = dao.add(city);
 			System.out.println("Se ha insertado" + n);
+
+			List<City> Cities = dao.getAll();
+			if (Cities.isEmpty()) {
+				System.out.println("No hay ciudades");
+			} else {
+				Cities.forEach(System.out::println);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void testDaoListar() {
+		CityDao dao = CityDaoImpl.getInstance();
+
+		try {
+			List<City> Cities = dao.getAll();
+			if (Cities.isEmpty()) {
+				System.out.println("No hay ciudades");
+			} else {
+				Cities.forEach(System.out::println);
+			}
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	public static void testDaoGetId() {
+		CityDao dao = CityDaoImpl.getInstance();
+
+		try {
+			City City = dao.getById(1);
+			System.out.println(City);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
+	public static void testUpdate() {
+		CityDao dao = CityDaoImpl.getInstance();
+
+		try {
+			City city = dao.getById(1);
+			city.setName("Not-Kabul");
+			dao.update(city);
+			city = dao.getById(1);
+			System.out.println(city);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void testBorrar() {
+		CityDao dao = CityDaoImpl.getInstance();
+
+		try {
+			dao.delete(4080);
+			System.out.println("Se borro");
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }

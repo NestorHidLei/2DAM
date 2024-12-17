2eb311f4a19814dff3c31aa30a868444a400c92f
Nestor <nhidlei2602@g.educaand.es>
2024-12-17 13:58:01 +0100

    Hauwk tuah

--- Examen JDBC y ficheros (Parte 1)/src/main/java/mainApp/MainApp.java
+++ Examen JDBC y ficheros (Parte 1)/src/main/java/mainApp/MainApp.java
@@ -3,11 +3,11 @@ package mainApp;
 import utils.UtilsBaseDatos;
 import utils.UtilsBaseDatosParte2;
 
-public class MainApp extends UtilsBaseDatos{
+public class MainApp extends UtilsBaseDatosParte2{
 	public static void main(String[] args) throws Exception {
 //		crearTablaPeliculas();
 //		insertarRegistrosTablaPeliculas();
-		cargarPeliculas(RUTA_FICHERO_NORMAL);
+//		cargarPeliculas(RUTA_FICHERO_NORMAL);
 //		peliculasToFichero("English", true);
 //		peliculasToFichero("English", false);
 
